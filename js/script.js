80f27dd2b04da7841fd03de49bc517f20e162551
Isabella Viana <bellakaroline1@gmail.com>
2023-08-20 17:08:54 -0400

    adicionada paginacao por slide

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 import { SlideNav } from './slide.js';
 
-const slide = new SlideNav(".slide", ".slide-wrapper");
+const slide = new SlideNav('.slide', '.slide-wrapper');
 slide.init();
 slide.addArrow('.prev', '.next');
+
+slide.addControl();
